24f3c274e2ad219c4506c9deb47dd692353f92e3
Daennes <alt.dennis@freenet.de>
2017-02-25 14:09:21 +0100

    Commit_1

---
+++ MA_Project/Sketch1/Sketch1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Sketch1.ino
+ Created:	25.02.2017 14:08:23
+ Author:	Dennis
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
