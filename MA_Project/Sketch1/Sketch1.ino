5863e222fea84d7ec37625a6d914ee7ffd1b7ff2
Daennes <alt.dennis@freenet.de>
2017-03-09 22:27:29 +0100

    Alphabet erweitert

--- MA_Project/Sketch1/Sketch1.ino
+++ MA_Project/Sketch1/Sketch1.ino
@@ -17,12 +17,24 @@ int latchPin_2 = 8;
 int clockPin_2 = 9;
 int dataPin_2 = 7;
 
-int zeichenKette[2];
+int zeichenKette[8];
 
 byte font[3][2] = {
 	{ 0x05, 0x02 },  
 	{ 0x02, 0x05 },  
 	{ 0x07, 0x04 }, 
+};
+byte fontZahlen[10][8] = {
+	{ 0x07, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x07 },		//0
+	{ 0x01, 0x01, 0x01, 0x01, 0x01, 0x05, 0x03, 0x01 },		//1
+	{ 0x07, 0x04, 0x04, 0x04, 0x07, 0x01, 0x01, 0x07 },		//2
+	{ 0x07, 0x01, 0x01, 0x01, 0x07, 0x01, 0x01, 0x07 },		//3
+	{ 0x01, 0x01, 0x01, 0x01, 0x07, 0x05, 0x05, 0x05 },		//5
+	{ 0x07, 0x01, 0x01, 0x01, 0x07, 0x04, 0x04, 0x07 },		//5
+	{ 0x07, 0x05, 0x05, 0x05, 0x07, 0x04, 0x04, 0x07 },		//6
+	{ 0x01, 0x01, 0x01, 0x01, 0x01, 0x05, 0x05, 0x07 },		//7
+	{ 0x07, 0x05, 0x05, 0x05, 0x07, 0x05, 0x05, 0x07 },		//8
+	{ 0x07, 0x01, 0x01, 0x01, 0x07, 0x05, 0x05, 0x07 },		//9
 };
 
 // the setup function runs once when you press reset or power the board
@@ -207,6 +219,184 @@ void Lauflicht2()
 			Zeichen2(i);
 	}
 }
+
+//String max 4 Zeichen, da Int
+void musterInitZ(String zeichen)
+{
+	Serial.println("Zeichenlange:");
+	Serial.println(zeichen.length());
+
+	for (int i = 0; i < 8; i++)
+	{
+		zeichenKette[i] = 0;
+	}
+					//Anzahl Zeilen --> Zeilen
+	for (int j = 0; j < 8; j++)
+	{
+		Serial.println("Durchgang: ");
+		for (int i = 0; i < zeichen.length(); i++)
+		{
+			if (zeichen[i] == '0')
+			{
+				if (i == 0)
+					zeichenKette[j] = font[0][j];
+				else {
+					zeichenKette[j] = zeichenKette[j] << 4;
+					zeichenKette[j] += font[0][j];
+				}
+
+				Serial.println(zeichenKette[j], BIN);
+				//break;
+			}
+			else if (zeichen[i] == '1')
+			{
+				if (i == 0)
+					zeichenKette[j] = font[1][j];
+				else {
+					zeichenKette[j] = zeichenKette[j] << 4;
+					zeichenKette[j] += font[1][j];
+				}
+				Serial.println(zeichenKette[j], BIN);
+				//break;
+			}
+			else if (zeichen[i] == '2')
+			{
+				if (i == 0)
+					zeichenKette[j] = font[2][j];
+				else {
+					zeichenKette[j] = zeichenKette[j] << 4;
+					zeichenKette[j] += font[2][j];
+				}
+				Serial.println(zeichenKette[j], BIN);
+				//break;
+			}
+			else if (zeichen[i] == '3')
+			{
+				if (i == 0)
+					zeichenKette[j] = font[3][j];
+				else {
+					zeichenKette[j] = zeichenKette[j] << 4;
+					zeichenKette[j] += font[3][j];
+				}
+				Serial.println(zeichenKette[j], BIN);
+				//break;
+			}
+			else if (zeichen[i] == '4')
+			{
+				if (i == 0)
+					zeichenKette[j] = font[4][j];
+				else {
+					zeichenKette[j] = zeichenKette[j] << 4;
+					zeichenKette[j] += font[4][j];
+				}
+				Serial.println(zeichenKette[j], BIN);
+				//break;
+			}
+			else if (zeichen[i] == '5')
+			{
+				if (i == 0)
+					zeichenKette[j] = font[5][j];
+				else {
+					zeichenKette[j] = zeichenKette[j] << 4;
+					zeichenKette[j] += font[5][j];
+				}
+				Serial.println(zeichenKette[j], BIN);
+				//break;
+			}
+			else if (zeichen[i] == '6')
+			{
+				if (i == 0)
+					zeichenKette[j] = font[6][j];
+				else {
+					zeichenKette[j] = zeichenKette[j] << 4;
+					zeichenKette[j] += font[6][j];
+				}
+				Serial.println(zeichenKette[j], BIN);
+				//break;
+			}
+			else if (zeichen[i] == '7')
+			{
+				if (i == 0)
+					zeichenKette[j] = font[7][j];
+				else {
+					zeichenKette[j] = zeichenKette[j] << 4;
+					zeichenKette[j] += font[7][j];
+				}
+				Serial.println(zeichenKette[j], BIN);
+				//break;
+			}
+			else if (zeichen[i] == '8')
+			{
+				if (i == 0)
+					zeichenKette[j] = font[8][j];
+				else {
+					zeichenKette[j] = zeichenKette[j] << 4;
+					zeichenKette[j] += font[8][j];
+				}
+				Serial.println(zeichenKette[j], BIN);
+				//break;
+			}
+			else if (zeichen[i] == '9')
+			{
+				if (i == 0)
+					zeichenKette[j] = font[9][j];
+				else {
+					zeichenKette[j] = zeichenKette[j] << 4;
+					zeichenKette[j] += font[9][j];
+				}
+				Serial.println(zeichenKette[j], BIN);
+				//break;
+			}
+
+
+		}
+	}
+
+	Serial.println("zeichenkette1:");
+	Serial.println(zeichenKette[0], BIN);
+	Serial.println(zeichenKette[1], BIN);
+	Serial.println("zeichenkette1_ende:");
+
+}
+
+void musterInitZ_test(String zeichen)
+{
+	Serial.println("Zeichenlange:");
+	Serial.println(zeichen.length());
+
+	for (int i = 0; i < 8; i++)
+	{
+		zeichenKette[i] = 0;
+	}
+	//Anzahl Zeilen --> Zeilen
+	for (int j = 0; j < 8; j++)
+	{
+		Serial.println("Durchgang: ");
+		for (int i = 0; i < zeichen.length(); i++)
+		{
+
+				if (i == 0)
+					zeichenKette[j] = font[(int)zeichen[i]][j];
+				else {
+					zeichenKette[j] = zeichenKette[j] << 4;
+					zeichenKette[j] += font[(int)zeichen[i]][j];
+				}
+
+				Serial.println(zeichenKette[j], BIN);
+
+			
+
+
+		}
+	}
+
+	Serial.println("zeichenkette1:");
+	Serial.println(zeichenKette[0], BIN);
+	Serial.println(zeichenKette[1], BIN);
+	Serial.println("zeichenkette1_ende:");
+
+}
+
 
 // the loop function runs over and over again until power down or reset
 void loop() {
